bb2c2be8b918f527e08a492a9e37fda534d241dd
Matheus <matheusyagami147@gmail.com>
2021-09-24 16:01:37 -0300

    criado diretório de app dentro de src. Diretório de models movido para dentro de app

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,10 @@
 const routes = require('express').Router()
+const { User } = require('./app/models')
+
+User.create({
+    name: 'matheeus', 
+    email: 'matheus1337@gmail.com',
+    password_hash: '21872339283257'
+})
 
 module.exports = routes
