3f12bd19c6b2f4ff1fab1afd2406d2ff6de0f9e2
Matheus <matheusyagami147@gmail.com>
2021-09-24 15:36:38 -0300

    banco de dados configurado e criada a migration create-users

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'matheus',
+  password: '123456',
+  database: 'test-jest',
+  dialect: 'postgres',
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
