ea81cdc8ed16fd2ed75d618887db4003e268fc38
Matheus <matheusyagami147@gmail.com>
2021-09-25 22:27:09 -0300

    criado o teste de retornar o jwt quando autenticado

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,48 +1,56 @@
-const request = require('supertest')
+const request = require("supertest");
 
+const app = require("../../src/app");
+const truncate = require("../utils/truncate");
+const factory = require("../factories");
 
-const app = require('../../src/app')
-const { User } = require('../../src/app/models')
-const truncate = require('../utils/truncate')
+describe("Authentication", () => {
+  beforeEach(async () => {
+    await truncate();
+  });
 
-describe('Authentication', () => {
-    beforeEach(async () => {
-        await truncate()
-    })
+  it("should authenticate with valid credentials", async () => {
+    const user = await factory.create("User", {
+      password: "123123"
+    });
 
-    it('should authenticate with valid credentials', async () => {
-        const user = await User.create({
-            name: "Matheus",
-            email: "matheusnunes1337@gmail.com",
-            password_hash: "123267"
-        })
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123"
+      });
 
-        const response = await request(app)
-        .post('/sessions')
-        .send({
-            email: user.email,
-            password: '123267'
-        })
-        
-        expect(response.status).toBe(200)
-        
-    })
+    expect(response.status).toBe(200);
+  });
 
-    it('should not authenticate with invalid credentials', async () => {
-        const user = await User.create({
-            name: "Matheus",
-            email: "matheusnunes1337@gmail.com",
-            password_hash: "123267"
-        })
+  it("should not authenticate with invalid credentials", async () => {
+    const user = await factory.create("User", {
+      password: "123123"
+    });
 
-        const response = await request(app)
-        .post('/sessions')
-        .send({
-            email: user.email,
-            password: '423267'
-        })
-        
-        expect(response.status).toBe(401)
-        
-    })
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123456"
+      });
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should return jwt token when authenticated", async () => {
+    const user = await factory.create("User", {
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123"
+      });
+
+    expect(response.body).toHaveProperty("token");
+  })
 })
